cd0127ce1dd8a8ed575e6f1fc8aea8837dfe1545
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-06-09 23:55:53 +0300

    Add validationSchema

--- justice/declaration.d.ts
+++ justice/declaration.d.ts
@@ -1,5 +1,4 @@
-declare module '*.scss';
-declare module '*.png';
-declare module '*.svg';
-
-
+declare module "*.scss";
+declare module "*.png";
+declare module "*.svg";
+declare module "*yup";
--- justice/src/components/RegisterPage/RegisterPage.tsx
+++ justice/src/components/RegisterPage/RegisterPage.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import {NavLink} from "react-router-dom";
-import {useNavigate} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import {Formik} from "formik";
-import * as yup from 'yup'
+import { Formik } from "formik";
+
+import * as yup from "yup";
 
 import axios from "axios";
 
@@ -18,11 +19,21 @@ import google from "../../assets/image/google.svg";
 import github from "../../assets/image/github.svg";
 
 const RegisterPage = () => {
+  const validationsSchema = yup.object().shape({
+    name: yup.string().typeError("Должно быть строкой").required("Обязательно"),
+    password: yup
+      .string()
+      .typeError("Должно быть паролем")
+      .required("Обязательно"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref(`password`)], "Нет совпадений")
+      .required("Обязательно"),
+  });
+
   const [checked, setChecked] = React.useState<boolean>(false);
-  const [disabledBtn, setDisabledBtn] = useState<boolean>(true);
 
   const navigate = useNavigate();
-
 
   const registration = () => {
     // axios.post("http://localhost:5000/api/auth/register-page", {
@@ -37,14 +48,11 @@ const RegisterPage = () => {
     // });
   };
 
-
   return (
     <main className={classes.main}>
       <div className={classes.main__register}>
         <div className={classes.main__register_wrapper}>
           <div className={classes.main__register_wrapper__form}>
-
-
             <p className={classes.main__register_wrapper__form_text}>
               Регистрация
             </p>
@@ -98,37 +106,112 @@ const RegisterPage = () => {
             </div>
             <div></div>
             <div className={classes.line_wrapper}>
-              <div className={classes.line}/>
+              <div className={classes.line} />
               <p className={classes.line__text}>Or</p>
-              <div className={classes.line}/>
+              <div className={classes.line} />
             </div>
             <div className={classes.input_wrapper}>
-              <Formik initialValues={{
-                name: '',
-                email: '',
-                password: '',
-                repeatPassword: ''
-              }} onSubmit={(values) => {
-                console.log(values)
-              }}>
+              <Formik
+                initialValues={{
+                  name: "",
+                  email: "",
+                  password: "",
+                  confirmPassword: "",
+                }}
+                onSubmit={(values) => {
+                  console.log(values);
+                }}
+                validationSchema={validationsSchema}
+              >
                 {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    isValid,
-                    handleSubmit,
-                    dirty
-                  }) => (
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  isValid,
+                  handleSubmit,
+                  dirty,
+                }) => (
                   <>
-                    <p>
-                      
-                    </p>
+                    <div className={classes.input_wrapper}>
+                      {touched.name && errors.name && <p>{errors.name}</p>}
+                      <Input
+                        className={classes.input}
+                        name={`name`}
+                        placeholder="Имя, Фамилия"
+                        type={`text`}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.name}
+                      />
+                    </div>
+                    {touched.password && errors.password && (
+                      <p>{errors.password}</p>
+                    )}
+                    <div className={classes.input_wrapper_password}>
+                      <Input
+                        className={classes.input}
+                        name={`password`}
+                        placeholder="Пароль"
+                        type={`password`}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                      />
+                      {touched.confirmPassword && errors.confirmPassword && (
+                        <p>{errors.confirmPassword}</p>
+                      )}
+
+                      <Input
+                        className={classes.input}
+                        name={`confirmPassword`}
+                        placeholder="Подтвердите пароль"
+                        type={`password`}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.confirmPassword}
+                      />
+                    </div>
+                    <div className={classes.desktop_button}>
+                      <ButtonMui
+                        text="Зарегистрироваться"
+                        padding="12px 180px"
+                        bc="#363636"
+                        coloring="#FFFFFF"
+                        onClick={() => registration()}
+                        disabled={!isValid && !dirty}
+                        fontWeight="600"
+                        hb="#363636"
+                        fontSize="16px"
+                      />
+                    </div>
+                    <div className={classes.mobile_button}>
+                      <ButtonMui
+                        text="Зарегистрироваться"
+                        padding="12px 80px"
+                        bc="#363636"
+                        coloring="#FFFFFF"
+                        onClick={handleSubmit}
+                        type={`submit`}
+                        disabled={!isValid && !dirty}
+                        fontWeight="600"
+                        hb="#363636"
+                        fontSize="16px"
+                      />
+                    </div>
                   </>
                 )}
-
               </Formik>
+              <div className={classes.newPerson}>
+                <p>
+                  {" "}
+                  У вас уже есть учетная запись?{" "}
+                  <NavLink to="/login-page" className={classes.signup}>
+                    Авторизоваться
+                  </NavLink>
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -139,7 +222,7 @@ const RegisterPage = () => {
           <p className={classes.text_regular}>Finance</p>
         </NavLink>
         <div className={classes.main__image_wrapper}>
-          <img src={image} alt="Register"/>
+          <img src={image} alt="Register" />
         </div>
       </div>
     </main>
