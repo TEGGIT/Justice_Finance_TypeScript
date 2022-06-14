d8f60c574ceb99ac854a74a53a533fee14e14b8d
TEGGIT <tolmachev.zhenya03@gmail.com>
2022-06-14 10:29:55 +0300

    Finish logic profile change

--- justice/src/components/Profile/Profile.tsx
+++ justice/src/components/Profile/Profile.tsx
@@ -18,221 +18,252 @@ import Cookies from "js-cookie";
 import classes from "./Profile.module.scss";
 
 type Inputs = {
-    name: string;
-    email: string;
-    city: string;
-    birthday: string;
-    phoneNumber: number;
-    oldPassword: string;
-    password:string;
+  name: string;
+  email: string;
+  city: string;
+  birthday: string;
+  phoneNumber: number;
+  oldPassword: string;
+  password: string;
 
 };
 
 const Profile = () => {
-    const {register, handleSubmit, watch, formState: {errors}} = useForm<Inputs>({mode: "onChange"});
-    const {users} = useTypedSelector((state) => state.user);
-    const {FetchUser} = useActions();
+  const {register, handleSubmit, watch, formState: {errors}} = useForm<Inputs>({mode: "onChange"});
+  const {users} = useTypedSelector((state) => state.user);
+  const {FetchUser} = useActions();
 
-    const changePassword = () => {
+  const changePassword = () => {
 
-    };
-    return (
-        <main className={classes.main}>
-            <NavBar/>
-            <section className={classes.main_wrapper}>
-                <div className={classes.main_wrapper__title}>
-                    <h1 className={classes.main_wrapper__title_text}>Мой профиль</h1>
-                    <form className={classes.main_wrapper__title_button} onSubmit={handleSubmit((data) => {
-                        axios.patch("http://localhost:5000/api/profile", {
-                                name: !data.name.length ? users[0]?.name : data.name,
-                                email: !data.email.length ? users[0]?.email : data.email,
-                                city: !data.city.length ? users[0]?.city : data.city,
-                                birthday: !data.birthday ? users[0]?.birthday : data.birthday,
-                                phoneNumber: !data.phoneNumber ? users[0].phoneNumber : data.phoneNumber
-                            },
-                            {
-                                headers: {Authorization: `${Cookies.get("TOKEN")}`},
-                            }
-                        )
-                            .then(() => {
-                                FetchUser();
+  };
+  return (
+    <main className={classes.main}>
+      <NavBar/>
+      <section className={classes.main_wrapper}>
+        <div className={classes.main_wrapper__title}>
+          <h1 className={classes.main_wrapper__title_text}>Мой профиль</h1>
+          <form className={classes.main_wrapper__title_button} onSubmit={handleSubmit((data) => {
+            axios.patch("http://localhost:5000/api/profile", {
+                name: !data.name.length ? users[0]?.name : data.name,
+                email: !data.email.length ? users[0]?.email : data.email,
+                city: !data.city.length ? users[0]?.city : data.city,
+                birthday: !data.birthday ? users[0]?.birthday : data.birthday,
+                phoneNumber: !data.phoneNumber ? users[0].phoneNumber : data.phoneNumber
+              },
+              {
+                headers: {Authorization: `${Cookies.get("TOKEN")}`},
+              }
+            )
+              .then(() => {
+                FetchUser();
 
-                            });
-                    })}>
-                        <ButtonMui
-                            bc="#363636"
-                            coloring="#FFFFFF"
-                            text="Сохранить изменения"
-                            padding="12px 24px"
-                            fontWeight="600"
-                            disabled=
-                                {
-                                    !watch(`name`)
-                                    &&
-                                    !watch(`email`)
-                                    &&
-                                    !watch(`city`)
-                                    &&
-                                    !watch(`birthday`)
-                                    &&
-                                    !watch(`phoneNumber`)
-                                }
-                            type="submit"
-                            fontSize="16px"
-                        />
-                    </form>
-                </div>
-                {/*{isSnackBar && (*/}
-                {/*  <CustomizedSnackbars snack={true}/>*/}
-                {/*  )}*/}
+              });
+          })}>
+            <ButtonMui
+              bc="#363636"
+              coloring="#FFFFFF"
+              text="Сохранить изменения"
+              padding="12px 24px"
+              fontWeight="600"
+              // disabled=
+              //   {
+              //     !watch(`name`)
+              //     &&
+              //     !watch(`email`)
+              //     &&
+              //     !watch(`city`)
+              //     &&
+              //     !watch(`birthday`)
+              //     &&
+              //     !watch(`phoneNumber`)
+              //   }
+              type="submit"
+              fontSize="16px"
+            />
+          </form>
+        </div>
+        {/*{isSnackBar && (*/}
+        {/*  <CustomizedSnackbars snack={true}/>*/}
+        {/*  )}*/}
 
-                <div className={classes.main_wrapper__content}>
-                    <div className={classes.main_wrapper__content__title__info}>
-                        <p>Информация о вашей учетной записи</p>
-                    </div>
-                    <form className={classes.main_wrapper__content__input}>
-                        <input
+        <div className={classes.main_wrapper__content}>
+          <div className={classes.main_wrapper__content__title__info}>
+            <p>Информация о вашей учетной записи</p>
+          </div>
+          <form className={classes.main_wrapper__content__input}>
+            <input
 
-                            placeholder="Имя, Фамилия"
-                            defaultValue={users[0]?.name}
-                            className={classes.main_wrapper__content__input_input}
-                            {...register(`name`, {
-                                pattern: {
-                                    value: /^[а-яА-ЯЁ ё]+$/,
-                                    message: 'Введено некорректное значение'
-                                }, minLength: {
-                                    value: 5,
-                                    message: 'Введено некорректное значение'
-                                }
-                            })}
+              placeholder="Имя, Фамилия"
+              defaultValue={users[0]?.name}
+              className={classes[`${!errors.name ? (`main_wrapper__content__input_input`) : (`main_wrapper__content__input_input_error`)}`]}
+              {...register(`name`, {
+                pattern: {
+                  value: /^[а-яА-ЯЁ ё]+$/,
+                  message: 'Введено некорректное значение'
+                }, minLength: {
+                  value: 5,
+                  message: 'Введено некорректное значение'
+                }
+              })}
 
-                        />
+            />
 
-                        {errors.name && (
-                            <><p>{errors.name.message}</p></>
-                        )}
-                        <input
-                            {...register(`email`)}
+            {errors.name && (
+              <><p>{errors.name.message}</p></>
+            )}
+            <input
+              {...register(`email`, {
+                pattern: {
+                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                  message: "Введено некорректное значение"
+                }
+              })}
+              placeholder="Email"
+              type="email"
+              defaultValue={users[0]?.email}
+              className={classes[`${!errors.email ? (`main_wrapper__content__input_input`) : (`main_wrapper__content__input_input_error`)}`]}
+            />
+            {errors.email && (
+              <><p>{errors.email.message}</p></>
+            )}
 
-                            placeholder="Email"
-                            type="email"
-                            defaultValue={users[0]?.email}
-                            className={classes.main_wrapper__content__input_input}
-                        />
+            <input
 
-                        <input
+              placeholder="Город"
+              className={classes[`${!errors.city ? (`main_wrapper__content__input_input`) : (`main_wrapper__content__input_input_error`)}`]}
+              {...register(`city`, {
+                pattern: {
+                  value: /^[а-яА-ЯЁ ё]+$/,
+                  message: "Введено некорректное значение"
+                },
+                minLength: {
+                  value: 3,
+                  message: "Введено некорректное значение"
+                }
+              })}
+              defaultValue={users[0]?.city}
+            />
+            {errors.city && (
+              <><p>{errors.city.message}</p></>
+            )}
 
-                            placeholder="Город"
-                            className={classes.main_wrapper__content__input_input}
-                            {...register(`city`)}
-                            defaultValue={users[0]?.city}
+            <input
+              placeholder="Дата рождения"
+              {...register(`birthday`, {
+                pattern: {
+                  value: /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/,
+                  message: "Введено некорректное значение"
+                }
+              })}
+              defaultValue={users[0]?.birthday}
+              className={classes[`${!errors.birthday ? (`main_wrapper__content__input_input`) : (`main_wrapper__content__input_input_error`)}`]}
+            />
+            {errors.birthday && (
+              <><p>{errors.birthday.message}</p></>
+            )}
 
+            <input
+              placeholder="Номер телефона"
+              className={classes[`${!errors.phoneNumber ? (`main_wrapper__content__input_input`) : (`main_wrapper__content__input_input_error`)}`]}
+              {...register(`phoneNumber`, {
+                  pattern: {
+                    value: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+                    message: 'Введено некорректное значение'
+                  }
+                }
+              )}
+              defaultValue={users[0]?.phoneNumber}
 
-                        />
+              type="number"
+            />
+            {errors.phoneNumber && (
+              <>
+                <p>{errors.phoneNumber.message}</p>
+              </>
+            )}
+            <div className={classes.main_wrapper__title_button_bottom}>
+              <ButtonMui
+                bc="#363636"
+                coloring="#FFFFFF"
+                text="Сохранить изменения"
+                padding="12px 24px"
+                hb="#363636"
+                fontWeight="600"
+                // disabled={isDisabled}
+                type="submit"
+                fontSize="12px"
+              />
+            </div>
+          </form>
 
-                        <input
-                            placeholder="Дата рождения"
-                            {...register(`birthday`)}
-                            defaultValue={users[0]?.birthday}
-                            className={classes.main_wrapper__content__input_input}
-                        />
+          <div className={classes.main_wrapper__content__title__password}>
+            <p>Пароль</p>
+          </div>
+          <form className={classes.main_wrapper__content__input} onSubmit={handleSubmit((data) => {
+            console.log(watch(`oldPassword`))
 
-                        <input
-                            placeholder="Номер телефона"
-                            className={classes.main_wrapper__content__input_input}
-                            {...register(`phoneNumber`, {minLength: {value: 11, message: 'Это не номер телефона'}})}
-                            defaultValue={users[0]?.phoneNumber}
+            axios.patch(
+              "http://localhost:5000/api/profile/changePassword", {
+                password: watch(`oldPassword`),
+                newPassword: watch(`password`),
+              },
+              {headers: {Authorization: `${Cookies.get("TOKEN")}`}}
+            )
+              .then(() => {
+                FetchUser();
+              }).catch(function () {
+              }
+            );
+          })}>
 
-                            type="number"
-                        />
-                        {errors.phoneNumber && (
-                            <>
-                                <p>{errors.phoneNumber.message}</p>
-                            </>
-                        )}
-                        <div className={classes.main_wrapper__title_button_bottom}>
-                            <ButtonMui
-                                bc="#363636"
-                                coloring="#FFFFFF"
-                                text="Сохранить изменения"
-                                padding="12px 24px"
-                                hb="#363636"
-                                fontWeight="600"
-                                // disabled={isDisabled}
-                                type="submit"
-                                fontSize="12px"
-                            />
-                        </div>
-                    </form>
+            <Input
+              placeholder="Введите старый пароль"
+              type="password"
+              {...register(`oldPassword`)}
+              className={classes.main_wrapper__content__input_input}
+              // onBlur={passwordChecker}
+              // value={oldPassword}
+              // onChange={(e) => setOldPassword(e.target.value)}
+            />
 
-                    <div className={classes.main_wrapper__content__title__password}>
-                        <p>Пароль</p>
-                    </div>
-                    <form className={classes.main_wrapper__content__input} onSubmit={handleSubmit((data) => {
-                        console.log(watch(`oldPassword`))
+            <Input
+              placeholder="Повторите новый пароль"
+              type="password"
+              className={classes.main_wrapper__content__input_input}
+              // onBlur={repeatsPassword}
+              // value={repeatPassword}
+              // onChange={(e) => setRepeatPassword(e.target.value)}
+            />
 
-                        axios.patch(
-                            "http://localhost:5000/api/profile/changePassword", {
-                                password: watch(`oldPassword`),
-                                newPassword: watch(`password`),
-                            },
-                            {headers: {Authorization: `${Cookies.get("TOKEN")}`}}
-                        )
-                            .then(() => {
-                                FetchUser();
-                            }).catch(function () {
-                            }
-                        );
-                    })}>
+            <Input
+              placeholder="Введите новый пароль"
+              type="password"
+              {...register(`password`)}
 
-                        <Input
-                            placeholder="Введите старый пароль"
-                            type="password"
-                            {...register(`oldPassword`)}
-                            className={classes.main_wrapper__content__input_input}
-                            // onBlur={passwordChecker}
-                            // value={oldPassword}
-                            // onChange={(e) => setOldPassword(e.target.value)}
-                        />
+              className={classes.main_wrapper__content__input_input}
+              // onBlur={newPassword}
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
+            />
 
-                        <Input
-                            placeholder="Повторите новый пароль"
-                            type="password"
-                            className={classes.main_wrapper__content__input_input}
-                            // onBlur={repeatsPassword}
-                            // value={repeatPassword}
-                            // onChange={(e) => setRepeatPassword(e.target.value)}
-                        />
-
-                        <Input
-                            placeholder="Введите новый пароль"
-                            type="password"
-                            {...register(`password`)}
-
-                            className={classes.main_wrapper__content__input_input}
-                            // onBlur={newPassword}
-                            // value={password}
-                            // onChange={(e) => setPassword(e.target.value)}
-                        />
-
-                        <ButtonMui
-                            bc="#363636"
-                            text="Изменить пароль"
-                            coloring="white"
-                            padding="15px 24px"
-                            hb="#363636"
-                            fontSize="16px"
-                            type='submit'
-                            // disabled={isDisabledPassword}
-                            fontWeight="600"
-                            onClick={changePassword}
-                        />
-                    </form>
-                </div>
-            </section>
-            <ProfileBar/>
-        </main>
-    );
+            <ButtonMui
+              bc="#363636"
+              text="Изменить пароль"
+              coloring="white"
+              padding="15px 24px"
+              hb="#363636"
+              fontSize="16px"
+              type='submit'
+              // disabled={isDisabledPassword}
+              fontWeight="600"
+              onClick={changePassword}
+            />
+          </form>
+        </div>
+      </section>
+      <ProfileBar/>
+    </main>
+  );
 };
 
 export default Profile;
